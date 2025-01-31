3ab3440d9ab0eb4ca7b77370b0eee3ec2aba80ba
AbdullahAlJubair <fabdullahjubair@gmail.com>
2025-01-31 21:06:38 +0600

    Added Updated JSON Data based on Requirement

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,16 +5,16 @@ const Card = ({ data }) => {
 
     return (
         <div className="p-5 md:p-4 lg:p-6 bg-white rounded-2xl flex flex-col gap-y-4 lg:gap-y-6">
-            <div className="border border-base-300 rounded-xl overflow-hidden">
-                <img className="w-full" src={product_image} alt="" />
+            <div className="max-h-[190px] border border-base-300 rounded-xl overflow-hidden">
+                <img className="w-full h-full object-cover" src={product_image} alt="" />
             </div>
             <div className="space-y-1 lg:space-y-4 grow">
-                <h3 className="BlackText text-lg md:text-xl lg:text-2xl font-semibold">{product_title}</h3>
-                <p className="GrayText text-sm md:text-base lg:text-xl font-medium">Price: ${price}</p>
+                <h3 className="BlackText text-base :text-lg font-semibold">{product_title}</h3>
+                <p className="GrayText text-sm md:text-base font-medium">Price: ${price}</p>
             </div>
             <div>
                 <Link to={`/product/${product_id}`}>
-                    <button className="AccentText text-sm lg:text-lg font-semibold AccentBorder rounded-4xl cursor-pointer py-2 px-5">View Details</button>
+                    <button className="AccentText text-sm font-semibold AccentBorder rounded-4xl cursor-pointer py-2 px-5">View Details</button>
                 </Link>
             </div>
 
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,10 +4,10 @@ const Categories = ({ categories }) => {
     return (
         <>
             <div className="grid grid-cols-2 md:flex md:flex-col gap-4 lg:gap-6 bg-white p-3 lg:p-6 rounded-2xl">
-                <NavLink className={({ isActive }) => `${isActive ? 'Accent text-white' : 'bg-[#09080f0d] GrayText'} py-2 lg:py-2.5 pl-4 md:px-5 rounded-4xl text-xs lg:text-lg`}
+                <NavLink className={({ isActive }) => `${isActive ? 'Accent text-white' : 'bg-[#09080f0d] GrayText'} py-2 lg:py-2.5 pl-4 md:px-5 rounded-4xl text-xs lg:text-base`}
                     to={'/'}>All Products</NavLink>
                 {
-                    categories.map(cat => <NavLink className={({ isActive }) => `${isActive ? 'Accent text-white' : 'bg-[#09080f0d] GrayText'} py-2 lg:py-2.5 pl-4 md:px-5 rounded-4xl text-xs lg:text-lg`}
+                    categories.map(cat => <NavLink className={({ isActive }) => `${isActive ? 'Accent text-white' : 'bg-[#09080f0d] GrayText'} py-2 lg:py-2.5 pl-4 md:px-5 rounded-4xl text-xs lg:text-base`}
                         key={cat.category}
                         to={`/category/${cat.category}`}>{cat.category}</NavLink>
                     )
